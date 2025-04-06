a7ef4ef8f807dfdb040e367f353f57c777fa5ab3
Jawad Hassan <171467245+jawadhasuna@users.noreply.github.com>
2025-04-06 08:36:12 +0500

    Create stop override.cpp

---
+++ stop override.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+class Employee{
+	private:
+	double salary;
+	string name;
+	int age;
+	public:
+		Employee(double s,string n,int a)
+		{
+		 	cout<<"constructor1"<<endl;
+		 salary=s;
+         name=n;
+	      age=a;	
+		}
+	void display()
+	{
+	cout<<salary<<name<<age<<endl;	
+	}
+	
+};
+	
+class shofer:public Employee{
+	private:
+		int id;
+		float km;
+   public:
+   	shofer(double s,string n,int a,int i,float k):Employee(s,n,a)
+		{
+		 	cout<<"constructor2"<<endl;
+		id=i;
+		km=k;	
+		}
+		 void display()
+		 	   {
+			   	 cout<<"hi";	      	
+				}
+};			
+main()
+{
+shofer s(345.3,"ali",20,1,45.2);
+//s.display();//over ride
+s.Employee::display(); //to stop over ride
+}
